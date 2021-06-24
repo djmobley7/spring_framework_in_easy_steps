905b7fbfa96ce0955a8adf9bda4f43663c4f283f
Daniel Mobley <djmobley7@gmail.com>
2021-06-24 09:47:44 -0400

    add example of how to close spring context

--- spring-core_advanced/src/main/java/com/bharath/spring/springcore_advanced/standalone_list/Test.java
+++ spring-core_advanced/src/main/java/com/bharath/spring/springcore_advanced/standalone_list/Test.java
@@ -1,6 +1,7 @@
 package com.bharath.spring.springcore_advanced.standalone_list;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
@@ -8,5 +9,7 @@ public class Test {
 		ApplicationContext context = new ClassPathXmlApplicationContext("com/bharath/spring/springcore_advanced/standalone_list/beans.xml");
 		Products products = (Products)context.getBean("products");
 		System.out.println(products);
+		
+		((AbstractApplicationContext)context).close();
 	}
 }
